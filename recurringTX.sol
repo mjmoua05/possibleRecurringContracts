0ffd4a301a3abd8eaef2c41e1ecaa0718be9d00c
mjmoua05 <68444965+mjmoua05@users.noreply.github.com>
2021-07-16 15:25:19 -0700

    Update recurringTX.sol

--- recurringTX.sol
+++ recurringTX.sol
@@ -18,12 +18,12 @@ contract recurTx {
 contract Recurring {
     address public owner;
     uint256 public userKey;
-    mapping(address => address) public userAcct;
+    mapping(address => address) public userAddress;
     mapping(uint256 => bytes32) public scheduledTxs;
 
     constructor () public {
         owner = msg.sender;
-    }    
+    }
 
     /* @param numBlock: block or timestamp at which the transaction should be executed. 
     @param to: recipient of the transaction.
@@ -33,18 +33,21 @@ contract Recurring {
     */
     
     // Schedule transactions
+    /* schedule info and values into scheduledTxs as hash and return the userKey and address
+    */
     function schedule(uint256 numBlock, address to, uint256 value, bytes memory data) public returns (uint,address){
         userKey = userKey + 1;
         scheduledTxs[userKey] = keccak256(abi.encodePacked(numBlock, msg.sender, to, value, data));
-        return (userKey,userAcct[msg.sender]);
+        return (userKey,userAddress[msg.sender]);
     }
 
      // execute transactions
     function executeTx(uint256 numBlock, address from, address to, uint256 value, bytes memory data) external {
         require(msg.sender==owner);
-        require(scheduledTxs[userKey]==keccak256(abi.encodePacked(numBlock, from, to, value, data)));
-        recurTx execTx = recurTx(userAcct[from]);
+        require(scheduledTxs[userKey]==keccak256(abi.encodePacked(numBlock, from, to, value, data))); //requires that the hash of data was saved in scheduledTxs
+        recurTx execTx = recurTx(userAddress[from]);
 
+        //execute the transactions/functions which is saved in data
         execTx.execFunct(to, value, data);
          
     }
