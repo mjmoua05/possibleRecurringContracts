1e028b3b813fd18f0ad2a0a7ab2e68528d584127
mjmoua05 <68444965+mjmoua05@users.noreply.github.com>
2021-07-16 14:57:07 -0700

    Update recurringMain.sol

--- recurringMain.sol
+++ recurringMain.sol
@@ -2,32 +2,32 @@ pragma solidity >=0.4.21;
 import './recurringTX.sol';
 
 contract RecurringTx{
-    function schedule(uint256 numBlock, address to, uint256 value, bytes memory data) public returns (uint,address);
-
+    function schedule(uint256 numBlock, address to, uint256 value, bytes memory data) virtual public returns (uint,address);
 }
 
 // Main contract
 contract MyContract{
-    RecurringTx recTx;
-    address addr; //= someAddressHere;
+    RecurringTx rTx;
+    address addr = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4; //= someAddressHere;
     uint256 timeToTx; // "insert time here - ex: 5 days"
-
-    constructor() public {
+    
+    constructor() public{
         scheduleTx();
     }
 
-    function scheduleTx() public {
-        recTx = RecurringTx(addr);
-        bytes memory data = abi.encodeWithSelector(bytes4(keccak256('transferTasks()')));
-        recTx.schedule(block.timestamp + timeToTx, address(this), 0, data);
-    }
-
-//function with tasks for transfer and actions to take when timer is up and then after call the function to reschedule again
+    //function with tasks for transfer and actions to take when time period is up and then after call the function to reschedule again
     function transferTasks() public {
-        //pay
-        //notify
+        //payFunction(); etc
         scheduleTx();
     }
+
+    //Schedule transactions call
+    function scheduleTx() public {
+        rTx = RecurringTx(addr);
+        bytes memory data = abi.encodeWithSelector(bytes4(keccak256('transferTasks()')));
+        rTx.schedule(block.timestamp + timeToTx, address(this), 0, data);
+    }
+
 
 
 }
